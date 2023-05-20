b874711d4977f0fb98a5933588965a08882caff8
arezoic <133676031+arezoic@users.noreply.github.com>
2023-05-20 18:54:27 +0700

    revert commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function hitungLuas() {
-
-
-    var sisiLuas = document.getElementById("sisi-luas").value;
-
-    document.getElementsByClassName("result-luas").style.visibilty="visible";
-    console.log(sisiLuas);
-    document.getElementById("output-luas").innerHTML = '<span>'
-}
