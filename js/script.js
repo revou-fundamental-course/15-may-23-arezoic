357d13bfe88a84d6041150b89db97d36c667ad23
arezoic <133676031+arezoic@users.noreply.github.com>
2023-05-20 18:56:14 +0700

    Luas Persegi

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function hitungLuas() {
+
+
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+    document.getElementsByClassName("result-luas").style.visibilty="visible";
+    console.log(sisiLuas);
+    document.getElementById("output-luas").innerHTML = '<span>'
+}
