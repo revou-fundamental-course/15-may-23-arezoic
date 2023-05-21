7af123c017af9ae3f6ea3cd64fac6a2829c0feb0
arezoic <133676031+arezoic@users.noreply.github.com>
2023-05-21 16:19:18 +0700

    keliling-persegi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
+
+var sisiLuas;
 function hitungLuas() {
+    sisiLuas = document.getElementById("sisi-luas").value;
+    var luasPersegi = sisiLuas * sisiLuas;
 
-
-    var sisiLuas = document.getElementById("sisi-luas").value;
-
-    document.getElementsByClassName("result-luas").style.visibilty="visible";
     console.log(sisiLuas);
-    document.getElementById("output-luas").innerHTML = '<span>'
+    document.getElementById("rumus-luas").innerHTML = 'L = S X S';
+    document.getElementById("perhitungan-luas").innerHTML = 'L = ' + sisiLuas +' x ' + sisiLuas;
+    document.getElementById("output-luas").innerHTML = 'L = ' + luasPersegi;
+}
+
+function resetLuas()  {
+    console.log(sisiLuas);
+    document.getElementById("rumus-luas").innerHTML = '';
+    document.getElementById("perhitungan-luas").innerHTML ='';
+    document.getElementById("output-luas").innerHTML ='';
+}
+
+var sisiKeliling;
+function hitungKeliling() {
+    sisiKeliling = document.getElementById("sisi-keliling").value;
+    var kelilingPersegi = 4 * sisiKeliling;
+
+    console.log(sisiKeliling);
+    document.getElementById("rumus-keliling").innerHTML = 'K = 4 X S';
+    document.getElementById("perhitungan-keliling").innerHTML = 'K = ' + '4 x ' + sisiKeliling;
+    document.getElementById("output-keliling").innerHTML = 'K = ' + kelilingPersegi;
+}
+
+function resetKeliling()  {
+    console.log(sisiKeliling);
+    document.getElementById("rumus-keliling").innerHTML = '';
+    document.getElementById("perhitungan-keliling").innerHTML ='';
+    document.getElementById("output-keliling").innerHTML ='';
 }
