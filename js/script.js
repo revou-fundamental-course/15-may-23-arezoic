fdd272bc4e0df095339b879e161df1a4f9509a03
arezoic <133676031+arezoic@users.noreply.github.com>
2023-05-23 18:15:42 +0700

    Add comment

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ function hitungDanCekLuas()   {
         hitungLuas(sisiLuas);
     }
 }
+// Variabel nya sisi luas, diambil dari html yang id nya sisi-luas. Jika sisi luas kosong, maka akan muncul notifikasi alert
+// Selain itu, maka luas akan di kalkulasi sesuai rumus sisi x sisi
+
 
 function hitungLuas() {
     var luasPersegi = sisiLuas * sisiLuas;
@@ -18,12 +21,14 @@ function hitungLuas() {
     document.getElementById("output-luas").innerHTML = 'L = ' + luasPersegi;
 }
 
+
 function resetLuas()  {
     console.log(sisiLuas);
     document.getElementById("rumus-luas").innerHTML = '';
     document.getElementById("perhitungan-luas").innerHTML ='';
     document.getElementById("output-luas").innerHTML ='';
 }
+// Jika mengklik reset luas, maka rumus-luas, perhitungan-luas, dan output-luas akan kosong
 
 var sisiKeliling;
 function hitungDanCekKeliling()   {
@@ -34,6 +39,8 @@ function hitungDanCekKeliling()   {
         hitungKeliling(sisiKeliling);
     }
 }
+// Variabel nya sisi keliling, diambil dari html yang id nya sisi-keliling. Jika sisi keliling kosong, maka akan muncul notifikasi alert
+// Selain itu, maka keliling akan di kalkulasi sesuai rumus 4 x sisi
 
 function hitungKeliling() {
     sisiKeliling = document.getElementById("sisi-keliling").value;
@@ -51,3 +58,4 @@ function resetKeliling()  {
     document.getElementById("perhitungan-keliling").innerHTML ='';
     document.getElementById("output-keliling").innerHTML ='';
 }
+// Jika mengklik reset keliling, maka rumus-keliling, perhitungan-keliling, dan output-keliling akan kosong
